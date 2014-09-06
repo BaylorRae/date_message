bdad47b9d1a5aaa6159eb5f3f96b33b56b0a51e1
Baylor Rae <baylorrae@gmail.com>
2014-09-06 10:17:21 -0500

    extract order date

--- spec/date_message_spec.rb
+++ spec/date_message_spec.rb
@@ -1,16 +1,17 @@
 require "./lib/date_message"
 
 describe DateMessage do
+  let(:order_date) { Date.new(2014, 1, 1) }
 
   it "defaults current date to today" do
-    date_message = DateMessage.new(Date.new(2014, 1, 1))
+    date_message = DateMessage.new(order_date)
     date_message.current_date.should == Date.today
   end
 
   context "today" do
     it "displays message for today" do
       today = Date.new(2014, 1, 1)
-      date_message = DateMessage.new(Date.new(2014, 1, 1), today)
+      date_message = DateMessage.new(order_date, today)
       date_message.message.should == "Last order was placed today"
     end
   end
@@ -18,7 +19,7 @@ describe DateMessage do
   context "yesterday" do
     it "displays message for order placed yesterday" do
       today = Date.new(2014, 1, 2)
-      date_message = DateMessage.new(Date.new(2014, 1, 1), today)
+      date_message = DateMessage.new(order_date, today)
       date_message.message.should == "Last order was placed yesterday"
     end
   end
@@ -26,7 +27,7 @@ describe DateMessage do
   context "two days ago" do
     it "displays message for order placed two days ago" do
       today = Date.new(2014, 1, 3)
-      date_message = DateMessage.new(Date.new(2014, 1, 1), today)
+      date_message = DateMessage.new(order_date, today)
       date_message.message.should == "Last order was placed 01/01/2014"
     end
   end
